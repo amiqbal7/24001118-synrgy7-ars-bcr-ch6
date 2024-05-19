060446300dd65554d76c630a64409f903e05724a
amiqbal <iqbalmuhammad7578@gmail.com>
2024-05-19 21:57:39 +0700

    delete dist

--- dist/utils/filter.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const filterCars = (arrList, idItem) => {
-    return arrList.find(({ id }) => id === idItem);
-};
-exports.default = filterCars;
-//# sourceMappingURL=filter.js.map
